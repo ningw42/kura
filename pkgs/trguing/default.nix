891873719b8ab9d8441955f56ae4399d4f223932
Ning Wang <unidentifiedme26@gmail.com>
2026-05-28 23:16:53 +0800

    feat(trguing): wire up updateScript

--- pkgs/trguing/default.nix
+++ pkgs/trguing/default.nix
@@ -1,14 +1,15 @@
 {
   stdenv,
   fetchzip,
+  nix-update-script,
 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation (finalAttrs: {
   pname = "trguing";
   version = "1.3.0";
 
   src = fetchzip {
-    url = "https://github.com/openscopeproject/TrguiNG/releases/download/v1.3.0/trguing-web-v1.3.0.zip";
+    url = "https://github.com/openscopeproject/TrguiNG/releases/download/v${finalAttrs.version}/trguing-web-v${finalAttrs.version}.zip";
     hash = "sha256-nX+SNzfWG9lMRBMooX33kp2S8eOr6UKtiLSjJoLq/us=";
     stripRoot = false;
   };
@@ -19,4 +20,11 @@ stdenv.mkDerivation {
     cp -r $src/* $out/share
     runHook postInstall
   '';
-}
+
+  passthru.updateScript = nix-update-script {
+    extraArgs = [
+      "--flake"
+      "--use-github-releases"
+    ];
+  };
+})
