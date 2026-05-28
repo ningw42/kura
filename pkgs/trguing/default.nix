07e59853d98de45baf70c6f2cfe54321826602a6
Ning Wang <unidentifiedme26@gmail.com>
2026-05-28 23:18:12 +0800

    build(trguing): update to `1.5.1`

--- pkgs/trguing/default.nix
+++ pkgs/trguing/default.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "trguing";
-  version = "1.3.0";
+  version = "1.5.1";
 
   src = fetchzip {
     url = "https://github.com/openscopeproject/TrguiNG/releases/download/v${finalAttrs.version}/trguing-web-v${finalAttrs.version}.zip";
-    hash = "sha256-nX+SNzfWG9lMRBMooX33kp2S8eOr6UKtiLSjJoLq/us=";
+    hash = "sha256-yiJN21fz3sgtm5YSkvR9DMGT3LuKXO5cLF/ELcN8a/4=";
     stripRoot = false;
   };
 
