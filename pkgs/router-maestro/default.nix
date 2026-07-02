c46cd06b65e819f5c547c6b61d6b5f96fca32d95
ningw42 <7892593+ningw42@users.noreply.github.com>
2026-07-02 03:28:02 +0000

    build(pkgs): update router-maestro, zashboard

--- pkgs/router-maestro/default.nix
+++ pkgs/router-maestro/default.nix
@@ -8,14 +8,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "router-maestro";
-  version = "0.3.28";
+  version = "0.4.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "MadSkittles";
     repo = "Router-Maestro";
     rev = "v${version}";
-    hash = "sha256-bPYrgJl/bgxUviNzwyYH3vpT3H/W0Q6ga8CoxW15LG0=";
+    hash = "sha256-r+ZRzkiys24pVoLOUjfZJh/3EWlBCIW2m5IPloujoAU=";
   };
 
   build-system = [ python3Packages.hatchling ];
