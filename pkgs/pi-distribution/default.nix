7ab04c80e47dcca58ccc0ceb78d98b4a6fbd5936
ningw42 <7892593+ningw42@users.noreply.github.com>
2026-09-11 11:08:36 +0000

    build(pkgs): update pi-distribution

--- pkgs/pi-distribution/default.nix
+++ pkgs/pi-distribution/default.nix
@@ -7,17 +7,17 @@
 
 buildNpmPackage (finalAttrs: {
   pname = "pi-distribution";
-  version = "26.09.6";
+  version = "26.09.7";
 
   src = fetchFromGitHub {
     owner = "ningw42";
     repo = "pi-distribution";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-v4Bt11aj7aghd5Qrqy2seOqktvv1ypV7CYBiDDSfDL0=";
+    hash = "sha256-yf1Ar0IFcm8JxqsD6dZPE1ny95CN1xL2VWNR3drpROY=";
   };
 
   npmDepsFetcherVersion = 2;
-  npmDepsHash = "sha256-8L1kqx1ZQxuOif2ZHhMhvOVhvDXE8MIZ3jTmTj0QBzM=";
+  npmDepsHash = "sha256-uL3cn0F+OLevBVnLMdAiPkwzXOiXFpOlKQtR/FhQ4qo=";
 
   # The package ships TypeScript extensions directly for Pi to load.
   dontNpmBuild = true;
