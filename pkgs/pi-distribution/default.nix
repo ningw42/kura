14bcbfcff8b98b32aaff8712d8a58d92744325ff
ningw42 <7892593+ningw42@users.noreply.github.com>
2026-09-19 00:42:28 +0000

    build(pkgs): update pi-distribution

--- pkgs/pi-distribution/default.nix
+++ pkgs/pi-distribution/default.nix
@@ -7,13 +7,13 @@
 
 buildNpmPackage (finalAttrs: {
   pname = "pi-distribution";
-  version = "26.09.17";
+  version = "26.09.18";
 
   src = fetchFromGitHub {
     owner = "ningw42";
     repo = "pi-distribution";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-NeSeLoxoOxex0kf47dQiNebaO16z+y19wuawM2JNHgE=";
+    hash = "sha256-1rLY5ISDWPOzkhmtQIB2nJaa+7CBcxems8UQM0RpPCU=";
   };
 
   # npm follows this short pkg.pr.new dependency edge instead of reusing the
